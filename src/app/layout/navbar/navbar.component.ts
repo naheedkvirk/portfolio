6209d0f6f2b79de9270adeb3388bd3346f5af345
Naheed Virk <naheedvirk@Naheeds-MacBook-Air.local>
2025-04-14 16:10:40 -0400

    Renamed the local project.

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar'; // Import Angular Material Toolbar
 import { HomeComponent } from '../../pages/home/home.component';
 import { RouterModule } from '@angular/router';
@@ -11,5 +11,5 @@ import { RouterModule } from '@angular/router';
   standalone: true,
 })
 export class NavbarComponent {
-
+@Input() authorName: string = '';
 }
