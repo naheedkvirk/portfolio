1995a207c259d96717e9cfb579cbdb2ac13bcec0
Naheed Virk <naheed.virk@gmail.com>
2026-07-21 22:43:48 -0400

    Create experiene data model

---
+++ components/Experience/Experience.tsx
@@ -0,0 +1,12 @@
+export interface Experience {
+  id: number;
+  company: string;
+  role: string;
+  duration: string;
+  location: string;
+  summary: string;
+  achievements: string[];
+  technologies: string[];
+}
+
+export const experience: Experience[] = [{}];
