64db97ae24343ff8031d0b1fd33d1a2358f6c977
Naheed Virk <naheed.virk@gmail.com>
2026-07-22 18:26:26 -0400

    Responsive styling and Timeline polish

--- components/Experience/ExperienceItem.tsx
+++ components/Experience/ExperienceItem.tsx
@@ -7,26 +7,60 @@ interface ExperienceItemProps {
 export default function ExperienceItem({ experience }: ExperienceItemProps) {
   return (
     <article className="relative pl-10">
-      {/* Timeline dot */}
-      <div className="absolute left-0 top-2 h-4 w-4 rounded-full bg-blue-600 ring-4 ring-blue-100 dark:ring-blue-900" />
-
       {/* Timeline line */}
-      <div className="absolute left-[7px] top-6 h-full w-px bg-gray-300 dark:bg-gray-700" />
+      <div className="absolute left-[7px] top-6 bottom-0 z-0 w-px bg-gray-300 dark:bg-gray-700" />
 
-      <div className="pb-12">
+      {/* Timeline dot */}
+      <div
+        className="
+          absolute
+          left-0
+          top-2
+          z-10
+          h-3
+          w-3
+          rounded-full
+          bg-blue-600
+          ring-4
+          ring-gray-50
+          dark:ring-gray-900
+        "
+      />
+
+      <div className="pb-16">
         {/* Header */}
         <header>
           <h3 className="text-2xl font-semibold text-gray-900 dark:text-white">
             {experience.role}
           </h3>
 
-          <p className="mt-1 text-lg font-medium text-blue-600 dark:text-blue-400">
+          <p className="mt-1 text-lg font-semibold text-blue-600 dark:text-blue-400">
             {experience.company}
           </p>
 
           <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">
             {experience.duration} • {experience.location}
           </p>
+
+          {experience.current && (
+            <span
+              className="
+                mt-3
+                inline-flex
+                rounded-full
+                bg-blue-100
+                px-3
+                py-1
+                text-xs
+                font-medium
+                text-blue-700
+                dark:bg-blue-900/40
+                dark:text-blue-300
+              "
+            >
+              Current Role
+            </span>
+          )}
         </header>
 
         {/* Summary */}
